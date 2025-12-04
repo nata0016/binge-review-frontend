8a3b5a1c40ffad3fdddb96ee7ca88a35960ae935
kinsy nyahim awassume <awass0011@algonquinlive.com>
2025-12-04 15:28:44 -0500

    done

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const api = axios.create({
-  baseURL: 'http://localhost:1337/api',
+const api = axios.create({
+  baseURL: "http://localhost:1337/api",
 });
+
+export const getReviews = async () => {
+  const res = await api.get("/reviews?populate=*");
+  return res.data.data;
+};
+
+export const getReviewBySlug = async (slug) => {
+  const res = await api.get(`/reviews?filters[slug][$eq]=${slug}&populate=*`);
+  return res.data.data[0];
+};
